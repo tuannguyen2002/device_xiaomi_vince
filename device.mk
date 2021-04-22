4b9cf1ab56a0416b1bb2870ae2685a05f38fda72
tuannguyen2002 <81414860+tuannguyen2002@users.noreply.github.com>
2021-04-22 16:45:36 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -328,8 +328,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service
-
+    android.hardware.power@1.2-service \
+    android.hardware.power-service-qti
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
