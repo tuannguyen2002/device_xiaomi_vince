cbc1d6b682d3f4b46043c7605280e49780c5f81b
tuannguyen2002 <81414860+tuannguyen2002@users.noreply.github.com>
2021-04-22 16:46:05 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_vince.mk
+    $(LOCAL_DIR)/ssos_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_vince-user \
-    derp_vince-userdebug \
-    derp_vince-eng
+    ssos_vince-user \
+    ssos_vince-userdebug \
+    ssos_vince-eng
